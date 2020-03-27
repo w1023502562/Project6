8d58fa88bef5ea3e973f04749bac91af6e1a404d
开心 王 <1023502562@qq.com>
2020-03-27 13:17:35 +0800

    4

---
+++ Project4.2/main.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+	cv::Mat srcMat = imread("D:\\coin.png", 0);
+	cv::Mat binaryMat;
+	cv::Mat labels;
+	cv::Mat stats;
+	cv::Mat centroids;
+
+	//二值化
+	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+	//创建结构元素
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	//闭运算
+	morphologyEx(binaryMat, binaryMat, MORPH_CLOSE, kernel);
+	//获得连通域
+	int liantongyu = connectedComponentsWithStats(binaryMat,
+		labels,
+		stats,
+		centroids,
+		8,
+		CV_32S);
+
+	//输出连通域信息
+
+	for (int i = 0; i < liantongyu; i++)
+	{
+		cout << "连通域序号： " << i << endl;
+		cout << "width = " << stats.at<int>(i, 2) << endl;
+		cout << "height = " << stats.at<int>(i, 3) << endl;
+		cout << "pixels = " << stats.at<int>(i, 4) << endl;
+
+	}
+	cout << "sum = " << liantongyu - 1 << endl;
+
+
+	//绘制bounding box
+	for (int i = 1; i < liantongyu; i++)
+	{
+
+		char num[10];
+		sprintf_s(num, "%d", i);
+
+		Rect boundingbox;
+		//bounding box左上角坐标
+		boundingbox.x = stats.at<int>(i, 0);
+		boundingbox.y = stats.at<int>(i, 1);
+		//bouding box的宽和长 
+		boundingbox.width = stats.at<int>(i, 2);
+		boundingbox.height = stats.at<int>(i, 3);
+		//绘制
+		rectangle(binaryMat, boundingbox, CV_RGB(255, 255, 255), 2, 8, 0);
+	}
+
+	imshow("binaryMat", binaryMat);
+
+	imshow("frame", srcMat);
+	waitKey(0);
+	return 0;
+}
---
+++ Project6/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cv::Mat srcMat = imread("D:\\coin.png", 0);
+	cv::Mat binaryMat;
+	cv::Mat erodeMat;
+	cv::Mat dilateMat;
+	cv::Mat openMat;
+	cv::Mat closeMat;
+	//¶þÖµ»¯
+	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+	imshow("binaryMat", binaryMat);
+	//´´½¨½á¹¹ÔªËØ¼°¸÷ÖÖÔËËã
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	morphologyEx(binaryMat, erodeMat, MORPH_ERODE, kernel);
+	morphologyEx(binaryMat, dilateMat, MORPH_DILATE, kernel);
+	morphologyEx(binaryMat, openMat, MORPH_OPEN, kernel);
+	morphologyEx(binaryMat, closeMat, MORPH_CLOSE, kernel);
+
+	imshow("erodeMat", erodeMat);
+	imshow("dilateMat", dilateMat);
+	imshow("openMat", openMat);
+	imshow("closeMat", closeMat);
+	waitKey(0);
+	return 0;
+}
